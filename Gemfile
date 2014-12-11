16e3c6faad68145b325475ec588fcbe56fcc7573
Sam Ruby <moft4studio@gmail.com>
2014-12-11 16:05:55 -0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,12 @@ gem 'rails'
 
 gem 'bcrypt',               '3.1.7'
 
+gem 'faker'
+
+gem 'will_paginate'
+
+gem 'bootstrap-will_paginate'
+
 gem 'bootstrap-sass',       '>= 3.2.0.0'
 # Use postgresql as the database for Active Record
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,12 @@ module SessionsHelper
   end
 
   # Returns the current logged-in user (if any).
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
-        if (user_id = session[:user_id])
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
@@ -39,5 +43,16 @@ module SessionsHelper
   	forget(current_user)
   	session.delete(:user_id)
   	@current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
